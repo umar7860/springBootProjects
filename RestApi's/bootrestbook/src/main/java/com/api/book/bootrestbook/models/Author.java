0fd3de6644ca5a8e055628c96d2aa237c32b1fa9
Umar <Umerqureshi800@gmail.com>
2023-09-20 10:49:03 +0500

    Implementing Custom Types and using in JPA

---
+++ RestApi's/bootrestbook/src/main/java/com/api/book/bootrestbook/models/Author.java
@@ -0,0 +1,64 @@
+package com.api.book.bootrestbook.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int authorID;
+    
+    private String firstName;
+    private String lastName;
+
+    private String language;
+
+    public Author() {
+    }
+
+    public Author(int authorID, String firstName, String lastName, String language) {
+        this.authorID = authorID;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.language = language;
+    }
+
+    public int getAuthorID() {
+        return authorID;
+    }
+
+    public void setAuthorID(int authorID) {
+        this.authorID = authorID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+
+}
--- RestApi's/bootrestbook/src/main/java/com/api/book/bootrestbook/models/Book.java
+++ RestApi's/bootrestbook/src/main/java/com/api/book/bootrestbook/models/Book.java
@@ -1,10 +1,12 @@
 package com.api.book.bootrestbook.models;
 
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -15,9 +17,11 @@ public class Book {
     private int bookId;
     // @Column(name = "Book_Author")
     private String title;
-    private String author;
 
-    public Book(int bookId, String title, String author) {
+    @OneToOne(cascade = CascadeType.ALL)
+    private Author author;
+
+    public Book(int bookId, String title, Author author) {
         this.bookId = bookId;
         this.title = title;
         this.author = author;
@@ -42,11 +46,11 @@ public class Book {
         this.title = title;
     }
 
-    public String getAuthor() {
+    public Author getAuthor() {
         return author;
     }
 
-    public void setAuthor(String author) {
+    public void setAuthor(Author author) {
         this.author = author;
     }
 
