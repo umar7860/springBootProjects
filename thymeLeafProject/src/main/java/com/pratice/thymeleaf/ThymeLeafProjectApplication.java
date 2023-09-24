b1881b7b04d76a1173dd1ad7aa6dc14acc42775d
Umar <Umerqureshi800@gmail.com>
2023-09-24 18:00:20 +0500

    Added Thymeleaf project

---
+++ thymeLeafProject/src/main/java/com/pratice/thymeleaf/ThymeLeafProjectApplication.java
@@ -0,0 +1,13 @@
+package com.pratice.thymeleaf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeLeafProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeLeafProjectApplication.class, args);
+	}
+
+}
---
+++ thymeLeafProject/src/test/java/com/pratice/thymeleaf/ThymeLeafProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pratice.thymeleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeLeafProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
